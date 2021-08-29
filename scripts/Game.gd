b140dcf494f16b48ac10ebdddaf7d476ffdd39cf
Miguel Medina Ballesteros <maximetinu@gmail.com>
2021-08-30 00:21:58 +0200

    Move onObstacleSpawned signal to Player

--- scripts/Game.gd
+++
@@ -1,4 +0,0 @@
-extends Spatial
-
-func _on_Obstacle_Spawner_obstacle_spawned(obstacle):
-	obstacle.connect("body_entered", $Player, "_on_Obstacle_body_entered")
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -14,6 +14,10 @@ func _process(delta):
 	if Input.is_action_just_pressed("jump") :
 		linear_velocity = Vector3.UP * jump_force
 	pass
+	
+	
+func _on_Obstacle_Spawner_obstacle_spawned(obstacle):
+	obstacle.connect("body_entered", self, "_on_Obstacle_body_entered")
 
 
 func _on_Obstacle_body_entered(body):
