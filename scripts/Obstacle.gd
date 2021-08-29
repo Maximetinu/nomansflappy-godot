7d5897a34cd48cb02f83506cdf0d9ef23dfb5761
Miguel Medina Ballesteros <maximetinu@gmail.com>
2021-08-30 00:23:34 +0200

    Clean code

--- scripts/Obstacle.gd
+++ scripts/Obstacle.gd
@@ -4,11 +4,5 @@ extends Area
 const speed = 5
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.translation = self.translation + Vector3.LEFT * speed * delta
--- scripts/ObstacleSpawner.gd
+++ scripts/ObstacleSpawner.gd
@@ -7,16 +7,6 @@ var obstaclePrefab = load("res://prefabs/Obstacle.tscn")
 var obstacleCount = 0
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
- 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Timer_timeout():
 	var obstacle = obstaclePrefab.instance()
 	obstacle.set_name("Obstacle " + str(obstacleCount))
