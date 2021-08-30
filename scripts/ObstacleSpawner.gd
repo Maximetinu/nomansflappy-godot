7709cbe584d3a20098f67a8365627bf8307b6265
Miguel Medina Ballesteros <maximetinu@gmail.com>
2021-08-30 12:54:43 +0100

    Randomize obstacle spawn

--- scripts/ObstacleSpawner.gd
+++ scripts/ObstacleSpawner.gd
@@ -3,12 +3,21 @@ extends Position3D
 
 signal obstacle_spawned(obstacle)
 
+const verticalRange = 7
+
 var obstaclePrefab = load("res://prefabs/Obstacle.tscn")
 var obstacleCount = 0
+
+var rng = RandomNumberGenerator.new()
+
+
+func _ready():
+	rng.randomize()
 
 
 func _on_Timer_timeout():
 	var obstacle = obstaclePrefab.instance()
 	obstacle.set_name("Obstacle " + str(obstacleCount))
+	obstacle.translation += Vector3.UP * rng.randf_range(-verticalRange/2, verticalRange/2)
 	add_child(obstacle)  
 	emit_signal("obstacle_spawned", obstacle)
