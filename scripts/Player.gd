ae3e65849978f4dca8a7a5563494961795b7b6e9
Miguel Medina Ballesteros <maximetinu@gmail.com>
2021-08-30 20:09:42 +0100

    Add score trigger

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -11,7 +11,12 @@ func _process(delta):
 	
 func _on_Obstacle_Spawner_obstacle_spawned(obstacle):
 	obstacle.connect("body_entered", self, "_on_Obstacle_body_entered")
+	obstacle.get_node("Score Trigger").connect("body_entered", self, "_on_Score_Trigger_body_entered")
 
 
 func _on_Obstacle_body_entered(body):
 	get_tree().reload_current_scene()
+	
+
+func _on_Score_Trigger_body_entered(body):
+	print("score++")
