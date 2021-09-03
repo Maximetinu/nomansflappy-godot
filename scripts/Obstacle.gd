3eb3fc184b9fc8bf39228a1463f9dc43552b77bb
Miguel Medina Ballesteros <maximetinu@gmail.com>
2021-09-03 23:23:54 +0100

    Destroy obstacle after time

--- scripts/Obstacle.gd
+++ scripts/Obstacle.gd
@@ -3,6 +3,10 @@ extends Area
 
 const speed = 3
 
+func _ready():
+	yield(get_tree().create_timer(10.0), "timeout")
+	queue_free()
+
 
 func _process(delta):
 	self.translation = self.translation + Vector3.LEFT * speed * delta
