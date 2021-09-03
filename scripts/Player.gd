05d4f7c3828b177a618071b6c39eddcd90f54735
Miguel Medina Ballesteros <maximetinu@gmail.com>
2021-09-03 23:21:09 +0100

    Player death animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -15,12 +15,18 @@ func _process(delta):
 	
 func _on_Obstacle_Spawner_obstacle_spawned(obstacle):
 	obstacle.connect("body_entered", self, "_on_Obstacle_body_entered")
-	obstacle.get_node("Score Trigger").connect("body_entered", self, "_on_Score_Trigger_body_entered")
+	obstacle.get_node("Score Trigger").connect("body_entered", self, "_on_Sc ore_Trigger_body_entered")
 
 
 func _on_Obstacle_body_entered(body):
-	get_tree().reload_current_scene()
-	
+	set_process(false)
+	self.gravity_scale = 0
+	self.linear_velocity = Vector3.ZERO
+	$CollisionShape.disabled = true
+	$Tween.interpolate_property(self, "scale", self.scale, self.scale * 35, 0.5, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$Tween.start()
+	yield($Tween, "tween_completed")
+	queue_free()
 
 func _on_Score_Trigger_body_entered(body):
 	score += 1
